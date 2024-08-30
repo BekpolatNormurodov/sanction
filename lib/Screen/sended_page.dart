22feedad55231aec6c8d0e939da51491ba4f5615
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2024-08-30 22:22:57 +0500

    sended page and search

--- lib/Screen/sended_page.dart
+++ lib/Screen/sended_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:animation_search_bar/animation_search_bar.dart';
 
 class SendedPage extends StatefulWidget {
   const SendedPage({super.key});
@@ -10,122 +11,163 @@ class SendedPage extends StatefulWidget {
 }
 
 class _SendedPageState extends State<SendedPage> {
+  late TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: Get.width - 280,
-      decoration: BoxDecoration(
-        color: Colors.grey.shade200,
-      ),
-      child: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Container(
-            margin: EdgeInsets.symmetric(horizontal: 50, vertical: 8),
-            padding: EdgeInsets.only(left: 20, right: 10),
-            width: Get.width,
-            height: 54,
-            decoration: BoxDecoration(
-              color: Colors.grey.shade50,
-              borderRadius: BorderRadius.circular(8),
-              boxShadow: [
-                BoxShadow(
-                  offset: Offset(1, 2),
-                  blurRadius: 5,
-                  color: Colors.black.withOpacity(.1)
-                )
-              ],
+    return Column(
+      children: [
+        AnimationSearchBar(
+          onChanged: (text) {
+            // names = _names!.where((e) {
+            //   return e.toLowerCase().contains(text.toLowerCase());
+            // }).toList();
+            // setState(() {});
+          },
+          searchTextEditingController: controller,
+          closeIconColor: Colors.blueAccent,
+          centerTitle: 'GUMONLANUVCHI SHAXSLAR',
+          hintText: 'Search here...',
+          centerTitleStyle: TextStyle(
+            color: Colors.grey.shade200,
+            fontSize: 14,
+            shadows: [
+              BoxShadow(
+                offset: Offset(.5, .5),
+                blurRadius: 1,
+                color: Colors.red.shade100,
+              )
+            ],
+          ),
+          hintStyle: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w300,
+          ),
+          textStyle: TextStyle(
+            fontWeight: FontWeight.w300,
+          ),
+          cursorColor: Colors.blueAccent.shade700,
+          duration: Duration(milliseconds: 300),
+          searchFieldHeight: 35,
+          searchBarHeight: 48,
+          searchBarWidth: Get.width - 300,
+          horizontalPadding: 5,
+          verticalPadding: 4,
+          searchIconColor: Colors.blueAccent,
+          searchFieldDecoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: Colors.blue.shade700,
+              width: 1,
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Icon(
-                      Icons.star_border_outlined,
-                      size: 24,
-                      color: Colors.black.withOpacity(.5),
-                    ),
-                    SizedBox(width: 16),
-                    Text(
-                      "Pul ko'paytirish:",
-                      style: GoogleFonts.slabo13px(
-                        textStyle: TextStyle(
-                          fontSize: 14,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: 20),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        Container(
+          width: Get.width - 280,
+          height: Get.height - 56,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade200,
+          ),
+          child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: EdgeInsets.only(
+                    left: 50, right: 50, bottom: 8, top: index == 0 ? 24 : 8),
+                padding: EdgeInsets.only(left: 20, right: 10),
+                width: Get.width,
+                height: 54,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade50,
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(1, 2),
+                        blurRadius: 4,
+                        color: Colors.black.withOpacity(.1)),
                   ],
                 ),
-                Row(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
-                      "Навбаҳор тумани ИИБ Навбатчилик қисмининг Шакл-1 китобида рўйхатга олинган ",
-                      style: GoogleFonts.slabo13px(
-                        textStyle: TextStyle(
-                          fontSize: 16,
-                          color: Colors.black,
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star_border_outlined,
+                          size: 24,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(width: 20),
+                        Text(
+                          "Pul ko'paytirish:",
+                          style: GoogleFonts.slabo13px(
+                            textStyle: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Навбаҳор тумани ИИБ Навбатчилик қисмининг Шакл-1 китобида рўйхатга олинган  ",
+                          style: GoogleFonts.slabo13px(
+                            textStyle: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          "19124",
+                          style: GoogleFonts.slabo13px(
+                            textStyle: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Text(
+                          " -сонли мурожаат",
+                          style: GoogleFonts.slabo13px(
+                            textStyle: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(),
+                    Container(),
+                    Container(),
+                    Container(),
+                    Container(),
+                    Container(
+                      alignment: Alignment.bottomRight,
+                      padding: EdgeInsets.only(bottom: 3),
+                      margin: EdgeInsets.only(left: 20),
+                      child: Text(
+                        "22.08.2024",
+                        style: GoogleFonts.robotoSerif(
+                          textStyle: TextStyle(
+                            fontSize: 13,
+                          ),
                         ),
                       ),
-                    ),
-                    Text(
-                      "19124",
-                      style: GoogleFonts.slabo13px(
-                        textStyle: TextStyle(
-                            fontSize: 16,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Text(
-                      "-сонли мурожаат",
-                      style: GoogleFonts.slabo13px(
-                        textStyle: TextStyle(
-                          fontSize: 16,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
+                    )
                   ],
                 ),
-                Container(),
-                Container(),
-                Container(),
-                Container(),
-                Container(),
-                Container(),
-                Container(),
-                Container(),
-                Container(),
-                Container(),
-                Container(),
-                Container(),
-                Container(),
-                Container(),
-                Container(),
-                Container(),
-                Container(),
-                Container(
-                  alignment: Alignment.bottomRight,
-                  padding: EdgeInsets.only(bottom: 3),
-                  margin: EdgeInsets.only(left: 20),
-                  child: Text(
-                    "22.08.2024",
-                    style: GoogleFonts.robotoSerif(
-                      textStyle: TextStyle(
-                        fontSize: 13,
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          );
-        },
-      ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
